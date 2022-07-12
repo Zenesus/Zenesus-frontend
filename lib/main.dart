7f3ff70587338b01a091a431ed9b100868ddcb00
EDED2314 <eddietang2314@gmail.com>
2022-07-12 15:52:21 -0400

    Improved Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,24 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+class Highschool{
+  late int id;
+  late String name;
 
-void main() {
+  Highschool(this.id, this.name);
+
+  static List<Highschool> getHighschools(){
+    return <Highschool>[
+      Highschool(0, "Select a School District"),
+      Highschool(1, "Montgomery")
+    ];
+  }
+
+
+}
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -14,36 +30,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Zenesus',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme:ThemeData.dark(),
+      home: const MyHomePage(title: 'Zenesus'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,79 +49,178 @@ class MyHomePage extends StatefulWidget {
 }
 
 
-//function to add border and rounded edges to our form
-OutlineInputBorder _inputformdeco(){
-  return OutlineInputBorder(
-    borderRadius: BorderRadius.circular(20.0),
-    borderSide:
-    BorderSide(width: 1.5, color: Colors.blue, style: BorderStyle.solid),
-  );
-}
+
 
 class _MyHomePageState extends State<MyHomePage> {
   String email = "";
   String password = "";
   String final_response = "";
 
-  final _formkey = GlobalKey<FormState>(); //key created to interact with the form
+  late bool _passwordVisible = false;
 
-  //function to validate and save user form
-  Future<void> _savingData() async{
-    final validation = _formkey.currentState?.validate();
-    if (!validation!){
-      return;
+  final List<Highschool> _highschools = Highschool.getHighschools();
+  late List<DropdownMenuItem<Highschool>> _dropdownMenuItems;
+  late Highschool _selectedHighschool;
+
+  @override
+  void initState() {
+    _dropdownMenuItems = buildDropdownMenuItems(_highschools);
+    _selectedHighschool = _dropdownMenuItems[0].value!;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<Highschool>> buildDropdownMenuItems(List<Highschool> highschools) {
+    List<DropdownMenuItem<Highschool>> items = [];
+    for (Highschool highschool in highschools){
+      items.add(
+        DropdownMenuItem(
+          value: highschool,
+          child: Text(highschool.name),
+        ),
+      );
     }
-    _formkey.currentState?.save();
+    return items;
+  }
+
+  onChangeDropdownItem(Highschool? selectedHighschool){
+    setState((){
+      _selectedHighschool = selectedHighschool!;
+    });
+  }
+
+  
+  final passwordController = TextEditingController();
+  final usernameController = TextEditingController();
+  
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    passwordController.dispose();
+    super.dispose();
+  }
+
+
+  //function to add border and rounded edges to our form
+  OutlineInputBorder _inputformdeco(){
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20.0),
+      borderSide:
+      BorderSide(width: 1.5, color: Colors.blue, style: BorderStyle.solid),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text("Zenesus",style:  TextStyle(
+            fontSize: 25,
+            letterSpacing: 7,
+            fontWeight: FontWeight.bold,
+            fontFamily: "Merriweather"
+        ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text(passwordController.text),
+              );
+            },
+          );
+        },
+        tooltip: 'Show me the value!',
+        child: const Icon(Icons.text_fields),
       ),
       body: Center(
 
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
 
           children: <Widget>[
-            const SizedBox(height: 100),
-            SizedBox(width: 350,
-              child: Form(key: _formkey,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Enter your email: ',
-                    enabledBorder: _inputformdeco(),
-                    focusedBorder: _inputformdeco(),
-                  ),onSaved: (value){
-                  setState(() {
-                    email = value!;
-                  });
-                },
-                ),
+            SizedBox(width: 350,height:450 ,
+              child:Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
 
+                  const Spacer(flex: 3),
+                  Image.asset('assets/open-book.png',
+                    height: 225,
+                    width: 225,
+                  ),
+
+                  const Spacer(flex: 1),
+
+                  DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.school),
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)
+                          )
+                      ),
+                      value: _selectedHighschool,
+                      items: _dropdownMenuItems,
+                      onChanged: onChangeDropdownItem
+                  ),
+
+                  const Spacer(flex: 1),
+                  TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: usernameController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: "Enter your email",
+                        enabledBorder: _inputformdeco(),
+                        focusedBorder: _inputformdeco(),
+                      )
+                    ),
+                  const Spacer(flex: 1),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    controller: passwordController,
+                    obscureText: !_passwordVisible,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                      enabledBorder: _inputformdeco(),
+                      focusedBorder: _inputformdeco(),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Theme.of(context).primaryColorDark,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const Spacer(flex: 1),
+
+                ],
+
+              )
               ),
-            ),
-            /*
-            Text(
-              name,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-
-            */
-            FlatButton(
+            ElevatedButton(
               onPressed: () async {
 
-                _savingData();
 
                 //url to send the get request to
-                const url = 'http://10.0.2.2:5000/api/getgrade';
+                const url = 'http://10.0.2.2:5000/api/login';
 
 
                 //sending a post request to the url
-                final post = await http.post(Uri.parse(url), body: json.encode({'email' : "asdfasdfhjk", "password":"asdfadsf"}));
+                final post = await http.post(Uri.parse(url), body: json.encode(
+                    {'email' : "asdfasdfhjk",
+                      "password":"asdfadsf", 'highschool':"Montgomery Highschool",
+                      "mp":"MP2"}));
 
                 //getting data from the python server script and assigning it to response
                 final response = await http.get(Uri.parse(url));
@@ -138,13 +235,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
               },
               child: Text('GET'),
-              color: Colors.lightBlue,
             ),
-            //Text(final_response, style: TextStyle(fontSize: 24)),
 
           ],
         ),
+
       ),
     );
   }
+
+
 }
