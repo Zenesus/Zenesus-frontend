a39e73a1249c6931a7be617bb922d9aa8dfb6705
EDED2314 <eddietang2314@gmail.com>
2022-07-20 17:52:56 -0400

    Log in screen redirection

--- lib/screens/courses.dart
+++ lib/screens/courses.dart
@@ -22,51 +22,86 @@ class _courses extends State<Courses>{
   @override
   void initState() {
     super.initState();
-    final String email = widget.password;
-    final String password = widget.email;
+    final String email = widget.email;
+    final String password = widget.password;
     final String school = widget.school;
 
     _fetchThings(email, password, school);
 
-    if(code['code']==69){
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const MyLoginPage(incorrect: true,)),
-      );
-    }
+
+
 
   }
 
   void _fetchThings(String email, String password, String school) async {
-    await Future.delayed(const Duration(seconds: 10));
     const url = 'http://10.0.2.2:5000/api/login';
 
-    try{
-      final post = await http.post(Uri.parse(url), body: json.encode(
+    final post = await http.post(
+        Uri.parse(url),
+        body: json.encode(
           {'email' : email,
-            "password":password, 'highschool':school,}));
+            "password": password,
+            'highschool':school,
+          }
+      )
+    );
 
       code = json.decode(post.body) as Map<String, dynamic>;
       if(code['code']==420){
         final response = await http.get(Uri.parse(url));
 
         decoded = json.decode(response.body) as Map<String, dynamic>;
+
+
+
       }
 
+      if(code['code']==69){
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MyLoginPage(incorrect: true,)),
+        );
+      }
 
-
-
-    }catch(error){
-
-      print(error);
-    }
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            "Zenesus",
+            style: TextStyle(
+              fontSize: 25,
+              letterSpacing: 7,
+              fontWeight: FontWeight.bold,
+              fontFamily: "Merriweather",
 
+            ),
+          ),
+        ),
+      body:Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+
+                  ElevatedButton(
+                      onPressed: (){
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MyLoginPage(incorrect: false,)),
+                          );
+                      },
+                      child: const Text('Go back')
+                  ),
+
+                    const CircularProgressIndicator()
+
+              ],
+          ),
+
+      ),
     );
   }
 
